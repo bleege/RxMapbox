58bac513315446bad1eea9c3eddba2df6cf34f68
Brad Leege <bleege@gmail.com>
2016-04-08 21:44:09 -0500

    Adding User Location functionality to FAB

--- app/src/main/java/com/bradleege/rxmapbox/MainActivity.java
+++ app/src/main/java/com/bradleege/rxmapbox/MainActivity.java
@@ -1,12 +1,20 @@
 package com.bradleege.rxmapbox;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -14,6 +22,12 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView = null;
+    private MapboxMap mapboxMap = null;
+
+    private FloatingActionButton fab = null;
+
+    // PERMISSION
+    public static final int PERMISSIONS_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +41,17 @@ public class MainActivity extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(MapboxMap mapboxMap) {
+            public void onMapReady(MapboxMap map) {
+                mapboxMap = map;
                 mapboxMap.setStyle(Style.MAPBOX_STREETS);
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                toggleGps(!mapboxMap.isMyLocationEnabled());
             }
         });
     }
@@ -70,5 +84,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    public void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                    (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                toggleLocationEnabled(true);
+            }
+        } else {
+            toggleLocationEnabled(false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    toggleLocationEnabled(true);
+                }
+            }
+        }
+    }
+
+    private void toggleLocationEnabled(boolean enabled) {
+        if (enabled) {
+            mapboxMap.setOnMyLocationChangeListener(new MapboxMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(@Nullable Location location) {
+                    if (location != null) {
+                        mapboxMap.setCameraPosition(new CameraPosition.Builder()
+                                .target(new LatLng(location))
+                                .zoom(16)
+                                .bearing(0)
+                                .tilt(0)
+                                .build());
+                        mapboxMap.setOnMyLocationChangeListener(null);
+                    }
+                }
+            });
+            fab.setImageResource(R.drawable.ic_gps_off_24dp);
+        } else {
+            fab.setImageResource(R.drawable.ic_gps_fixed_24dp);
+        }
+        mapboxMap.setMyLocationEnabled(enabled);
     }
 }
